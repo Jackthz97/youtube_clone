fc9c058961d2e2577fd6880d11cf3447732e85b8
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-07-28 14:15:42 -0400

    first commit setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+//https://youtube.googleapis.com/youtube/v3/search?part=snippet&order=relevance&q=batman&key=AIzaSyA2FjKL5XNYP8JtFhBc_au5dHbMMhuF4hQ
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
